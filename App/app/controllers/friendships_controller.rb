32e068123ae672b67d1fe6630dcc5694a9957f86
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-12-03 12:13:51 -0600

    se realiza buen acercaminto a la solucion de la etapa uno dos

--- App/app/controllers/friendships_controller.rb
+++ App/app/controllers/friendships_controller.rb
@@ -7,7 +7,13 @@ class FriendshipsController < ApplicationController
       config.access_token        = "1415674062-Tl1yzoynINYezB7KKYaGV10hdOch3EDnFqzI1rS"
       config.access_token_secret = "cXDzlTnV8C4q5VReYMQntn8S3YfExmR7KgOedw9ZHdluW"
     end
-    p client.followers("cggg88jorge")
+    # followers_cg = client.followers("cggg88jorge").map { |e| e.screen_name }
+    # followers_gz = client.followers("gzfrancisco").map { |e| e.screen_name }
+    followers_cg = %w[one two tree]
+    followers_gz = %w[one tree four]
+    p followers_gz & followers_cg
+    
+    p [{name: "one", following: true}, {name: "two", following: false}].select { |e| e[:following] }
   end
 
 end
